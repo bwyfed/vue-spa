da1695766d8453f41fc5a5ef8e0dbf637f49248c
Bianwy <bianwy025@qq.com>
2018-12-12 17:27:51 +0800

    尝试进行代码分割，并进行懒加载。

--- src/router.js
+++ src/router.js
@@ -1,6 +1,20 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
+// import Home from './views/Home.vue'
+
+// 引入 Home 组件
+const Home = resolve => {
+  require.ensure(['./views/Home.vue'], () => {
+    resolve(require('./views/Home.vue'))
+  })
+}
+
+// 引入 About 组件
+const About = resolve => {
+  require.ensure(['./views/About.vue'], () => {
+    resolve(require('./views/About.vue'))
+  })
+}
 
 Vue.use(Router)
 
@@ -16,10 +30,11 @@ export default new Router({
     {
       path: '/about',
       name: 'about',
+      component: About
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+      // component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
     }
   ]
 })
