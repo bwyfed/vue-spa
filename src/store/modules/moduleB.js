54f049faee1c39b2033e299aaa9f388ccd5d31ee
Bianwy <bianwy025@qq.com>
2018-12-13 09:30:14 +0800

    增加vuex的拆分和配置。

---
+++ src/store/modules/moduleB.js
@@ -0,0 +1,25 @@
+// 模块B
+
+const moduleB = {
+  state: {
+    text: 'B state'
+  },
+  mutations: {
+    addText (state, txt) {
+      // 这里的 `state`对象是模块的局部状态
+      state.text += txt
+    }
+  },
+  actions: {
+    setText ({ commit }) {
+      commit('addText', 'action')
+    }
+  },
+  getters: {
+    getText (state) {
+      return state.text + '!'
+    }
+  }
+}
+
+export default moduleB
